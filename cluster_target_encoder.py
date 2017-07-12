be199e6607984db59a534a4f495590dd7c6097ab
Danila89 <savenkovdanila@rambler.ru>
2017-07-12 17:36:38 +0300

    Add cluster encoder

---
+++ cluster_target_encoder.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+class cluster_target_encoder:
+    def __init__(self,seed=0):
+        self.seed = seed
+    def make_encoding(self,df):
+        self.encoding = df.groupby('X')['y'].mean()
+    def fit(self,X,y):
+        df = pd.DataFrame(columns=['X','y'],index=X.index)
+        df['X'] = X
+        df['y'] = y
+        self.make_encoding(df)
+        clust = KMeans(4,random_state=self.seed)
+        labels = clust.fit_predict(self.encoding[df['X'].values].values.reshape(-1,1))
+        df['labels'] = labels
+        self.clust_encoding = df.groupby('X')['labels'].median()
+    def transform(self,X):
+        res = X.map(self.clust_encoding).astype(float)
+        return res
+    def fit_transform(self,X,y):
+        self.fit(X,y)
+        return self.transform(X)
